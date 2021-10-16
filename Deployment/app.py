6175cd5373686eb489800060147213bd4da90a02
shh0277 <92477607+shh0277@users.noreply.github.com>
2021-10-15 20:36:38 -0700

    Add files via upload

---
+++ Deployment/app.py
@@ -0,0 +1,73 @@
+import torch, pytz
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import os, csv
+# For plotting
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+import streamlit as st
+from PIL import Image
+
+
+# Page config
+st.set_page_config(page_title="ARMOR Lab Human Digital Twin Demo", layout="wide")
+
+app_intro = """
+This website is designed for the demonstration of how functional human movement assessment can be done by using Motion Tape.
+For each model/movement, the results obtained from the motion capture system are served as ground truth values to train the model.
+Predictions are compared with the results obtained from the motion capture system to demonstrate the performance of Motion Tape.
+* __Some Text here__: Some Text here
+* __Some Text here__: Some Text here
+* __Some Text here__: Some Text here
+* __Some Text here__: Some Text here \n
+"""
+
+# Info
+st.header('Introduction')
+st.write(app_intro)
+st.write("")
+st.write("")
+
+image = Image.open("Picture1.png")
+image2 = Image.open("UCSDLogo_JSOE_Black.png")
+st.sidebar.image(image2, use_column_width=True)
+st.sidebar.image(image, use_column_width=True)
+
+# Load data
+
+with st.sidebar.expander('Models', expanded=True):
+    model_name = st.selectbox(
+        "Select a model",
+        options=('Bicep Curl', 'Squat'))
+    
+    file = st.file_uploader("Upload a csv file", type="csv")
+
+if model_name == "Bicep Curl":
+    if file:
+        st.write('File Received.')
+    else:
+        st.header('Example:')
+
+        st.subheader('Motion Capture System:')
+        video_file = open('Trial09.mp4', 'rb')
+        video_bytes = video_file.read()
+        st.video(video_bytes)
+
+        st.subheader('Voltage Responses from Motion Tape:')
+        image3 = Image.open('Trial09.jpg')
+        st.image(image3)
+
+        st.subheader('Prediction:')
+        col_left, col_mid, col_right = st.columns([2, 2, 1])
+        with col_mid:
+            image4 = Image.open('Trial9.png')
+            st.image(image4)
+elif model_name == "Squat":
+    if file:
+        st.write('File Received.')
+    else:
+        st.stop()
+
+
